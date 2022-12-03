47a03631030ffad06b532bf3d23d1c7a76200789
Elijah Zupancic <elijah@zupancic.name>
2022-12-03 13:19:13 -0800

    build: Fix homebrew formula

--- Formula/bunyan_view.rb
+++ Formula/bunyan_view.rb
@@ -1,12 +1,19 @@
 class BunyanView < Formula
-  desc "Cross-platform Text Expander written in Rust"
+  desc "A full-featured port of the Node Bunyan structured log file viewer"
   homepage "https://github.com/dekobon/bunyan-view"
-  url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-  sha256 "2473866b99eef9ea983200b7aac91592b938404ffaa1fb8c72beacb2ebd3203a"
-  version "0.2.1"
+  version "0.3.0"
+
+  if OS.mac? and Hardware::CPU.intel?
+      url "https://github.com/dekobon/bunyan-view/releases/download/v#{version}/bunyan-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "4df39716ae6024ab68d877343afcdd3ef6f70a175a4f2638900942ce211dfd13"
+  elsif OS.linux? and Hardware::CPU.intel?
+      url "https://github.com/dekobon/bunyan-view/releases/download/v#{version}/bunyan-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "aed5dc69a9fc5e9208c1fdb14237ae329dbfbaa6cb1c972767fe4da142cd5558"
+  end
+
 
   def install
     bin.install "bunyan"
-    man1.install "bunyan.1.gz"
+#    man1.install "bunyan.1.gz"
   end
 end
