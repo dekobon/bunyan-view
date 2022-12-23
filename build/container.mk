3531031da1b33ee922370159f79d5697a4ab01da
Elijah Zupancic <elijah@zupancic.name>
2022-12-22 16:19:08 -0800

    build: turned off interactive and tty on docker tasks

--- build/container.mk
+++ build/container.mk
@@ -7,32 +7,32 @@ container-debian-build-image: ## Builds a container image for building on Debian
 
 .PHONY: container-deb-packages
 container-deb-packages: container-debian-build-image ## Builds deb packages using a container image
-	$Q $(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder make deb-packages
+	$Q $(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder make deb-packages
 	# Reset permissions on the target directory to the current user
 	if command -v id > /dev/null; then \
-		$(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
+		$(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
 	fi
 
 .PHONY: container-rpm-packages
 container-rpm-packages: container-debian-build-image ## Builds a rpm packages using a container image
-	$Q $(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder make rpm-packages
+	$Q $(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder make rpm-packages
 	# Reset permissions on the target directory to the current user
 	if command -v id > /dev/null; then \
-		$(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
+		$(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
 	fi
 
 .PHONY: container-all-packages
 container-all-packages: container-debian-build-image ## Builds all packages using a container image
-	$Q $(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder make all-packages
+	$Q $(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder make all-packages
 	# Reset permissions on the target directory to the current user
 	if command -v id > /dev/null; then \
-		$(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
+		$(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
 	fi
 
 .PHONY: container-test
 container-test: container-debian-build-image ## Run tests inside container
-	$Q $(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder make test
+	$Q $(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder make test
 	# Reset permissions on the target directory to the current user
 	if command -v id > /dev/null; then \
-		$(DOCKER) run --rm --tty --interactive --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
+		$(DOCKER) run --rm --volume "$(CURDIR):/project" --workdir /project debian_builder chown --recursive "$(shell id -u):$(shell id -g)" /project/target
 	fi
