8b158210f2ea791d83b88003963df9c388d27e26
Elijah Zupancic <elijah@zupancic.name>
2022-12-24 13:20:45 -0800

    build: add github release workflow

--- build/release.mk
+++ build/release.mk
@@ -1,6 +1,6 @@
-LAST_VERSION       = $(shell git tag -l | $(GREP) -E '^v[0-9]+\.[0-9]+\.[0-9]+$$' | sort --version-sort --field-separator=. --reverse | head -n1 | $(SED) -e 's/^v//')
-LAST_VERSION_HASH  = $(shell git show --format=%H $(LAST_VERSION) | head -n1)
-CHANGES            = $(shell git log --format="%s	(%h)" "$(LAST_VERSION_HASH)..HEAD" | \
+LAST_VERSION       := $(shell git tag -l | $(GREP) -E '^v[0-9]+\.[0-9]+\.[0-9]+$$' | sort --version-sort --field-separator=. --reverse | head -n1 | $(SED) -e 's/^v//')
+LAST_VERSION_HASH  := $(shell git show --format=%H v$(LAST_VERSION) | head -n1)
+CHANGES            := $(shell git log --format="%s	(%h)" "$(LAST_VERSION_HASH)..HEAD" | \
 					 	$(GREP) -v '^(ci|chore): .*' | \
                         $(SED) 's/: /:\t/g1' | \
                         column -s "	" -t | \
@@ -10,8 +10,8 @@ CHANGES            = $(shell git log --format="%s	(%h)" "$(LAST_VERSION_HASH)..H
 .PHONY: changelog
 .ONESHELL: changelog
 changelog: ## Outputs the changes since the last version committed
-	$Q echo 'Changes since $(LAST_VERSION):'
-	$Q echo "$(CHANGES)" | tr '\1' '\n'
+	$Q echo 'Changes in $(VERSION) since $(LAST_VERSION):'
+	echo "$(CHANGES)" | tr '\1' '\n'
 
 .ONESHELL: target/dist/release_notes.md
 target/dist/release_notes.md: target/dist target/dist/SHA256SUMS
@@ -39,6 +39,16 @@ version-update: ## Prompts for a new version
 	$Q echo "  last committed version: $(LAST_VERSION)"
 	$Q echo "  Cargo.toml file version : $(VERSION)"
 	read -p "  Enter new version in the format (MAJOR.MINOR.PATCH): " version
-	$Q echo "$$version" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+$$' || \
+	$Q echo "$$version" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+-?.*$$' || \
 		(echo "invalid version identifier: $$version" && exit 1) && \
 	$(SED) -i "s/^version\s*=.*$$/version = \"$$version\"/" $(CURDIR)/Cargo.toml
+	@ VERSION=$(shell $(GREP) -Po '^version\s+=\s+"\K.*?(?=")' $(CURDIR)/Cargo.toml)
+
+.PHONY: version-release
+.ONESHELL: version-release
+version-release: ## Change from a pre-release to full release version
+	$Q echo "$(VERSION)" | grep -qE '^[0-9]+\.[0-9]+\.[0-9]+-beta$$' || \
+		(echo "invalid version identifier - must contain suffix -beta: $(VERSION)" && exit 1)
+	export NEW_VERSION="$(shell echo $(VERSION) | $(SED) -e 's/-beta$$//')"
+	$(SED) -i "s/^version\s*=.*$$/version = \"$$NEW_VERSION\"/" $(CURDIR)/Cargo.toml
+	@ VERSION=$(shell $(GREP) -Po '^version\s+=\s+"\K.*?(?=")' $(CURDIR)/Cargo.toml)
